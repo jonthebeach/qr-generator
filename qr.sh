df2576b193696983458316cf439a464195377b77
oscartt89 <oscart@uma.es>
2018-05-31 19:21:25 +0200

    Adding the header to the bash script

--- qr.sh
+++ qr.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#Copyright (c) 2018 Oscar Torreno
+#
+#This Source Code Form is subject to the terms of the Mozilla Public
+#License, v. 2.0. If a copy of the MPL was not distributed with this
+#file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 # Name of the temporary file
 INPUT="$1.tmp"
 OLDIFS=$IFS
